382329cbeeac0e029aece84ca2e339668aa11459
Wim Looman <ghostunderscore@gmail.com>
2011-04-15 20:12:17 +1200

    Added more comments and tidied the hypothesis building code a bit

--- lib/cea/hypothesis.rb
+++ lib/cea/hypothesis.rb
@@ -1,6 +1,7 @@
 module CEA
   module Private
     class Hypothesis
+      # +args+ should be a hash of attribute to value mappings.
       def initialize args
         @hash = args.dup
       end
@@ -25,35 +26,47 @@ module CEA
         @hash.inspect
       end
 
+      # A hypothesis covers an example or another hypothesis when this
+      # hypothesis has either the same value or :undefined for all attributes.
       def covers? other
         @hash.keys.all? do |attr|
           self[attr] == other[attr] || self[attr] == :undefined
         end
       end
 
+      # Finds all hypotheses that are one step more specific than this
+      # hypothesis.  Can simply replace all :undefined attributes one at a time
+      # with each of their possible values.
       def descendants
         @hash.select { |attr, value| value == :undefined }.map do |attr, value|
           attributes[attr].map do |value|
-            hash = @hash.dup
-            hash[attr] = value
-            self.class.new(hash)
+            self.class.new(@hash.merge({ attr => value }))
           end
         end.flatten
       end
 
+      # Finds all hypotheses that are one step more general than this
+      # hypothesis.  If this is the null hypothesis then construct the entire
+      # set of most specific hypotheses.  Otherwise simply generates all single
+      # replacements of an attribute with :undefined.
       def ancestors
-        # Builds all the combinations of attributes if it's the null hypothesis.
         if @hash.values.include? :null
           s = []
-          p = proc { |v, hs| v.empty? ? s << self.class.new(Hash[*hs]) : v[0].each { |k| p[v[1..v.length], hs + k] } }
-          p[attributes.map { |key, values| values.map { |value| [key, value] } }, []]
+          build_hash = proc do |attributes, current_hypothesis|
+            if attributes.empty?
+              s << self.class.new(Hash[*current_hypothesis])
+            else
+              attributes[0].each do |value|
+                build_hash[attributes[1..attributes.length], current_hypothesis + value]
+              end
+            end
+          end
+          build_hash[attributes.map { |key, values| values.map { |value| [key, value] } }, []]
           return s
-        end
-
-        @hash.select { |attr, value| value != :undefined }.map do |attr, value|
-          hash = @hash.dup
-          hash[attr] = :undefined
-          self.class.new(hash)
+        else
+          @hash.select { |attr, value| value != :undefined }.map do |attr, value|
+            self.class.new(@hash.merge({ attr => :undefined }))
+          end
         end
       end
     end
